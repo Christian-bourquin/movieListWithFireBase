61cfd209793bd7523dd893b6579986dadd9eeb16
Christian-bourquin <christian.bourquin23@d155.org>
2023-01-20 08:08:18 -0600

    go over and convert everything

--- movieListWithFireBase/ViewController.swift
+++ movieListWithFireBase/ViewController.swift
@@ -4,13 +4,55 @@
 //
 //  Created by CHRISTIAN BOURQUIN on 1/17/23.
 //
+class movies{
+    var name: String
+    var rating: Double
+    var ref = Database.database().reference()
+    var key = ""
+    init(name: String, rating: Double) {
+        self.name = name
+        self.rating = rating
+    }
+    init(dict: [String: Any]) {
+        if let n = dict["name"] as? String{
+            name = n
+        }
+        else{
+            name = ""
+        }
+        if let a = dict["rating"] as? Double{
+            rating = a
+        }
+        else {
+            rating = 0
+        }
+    }
+    func saveToFireBase(){
+        var dict = ["name":name,"rating":rating] as [String: Any]
+        key = ref.child("Movies").childByAutoId().key ?? ""
+        ref.child("Movies").child(key).setValue(dict)
+        
+    }
+    func deleteFromFirebase (){
+        ref.child("Movies").child(key).removeValue()
+    }
+    func updateFirebase(){
+        let dict = ["name": name,"rating": rating] as! [String: Any]
+        ref.child("Movies").child(key).updateChildValues(dict)
+        
+    }
+}
+
+
 
 import UIKit
 import FirebaseCore
 import FirebaseDatabase
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    var movies = [String]()
-    
+    var ref: DatabaseReference!
+    //var names = [String]()
+    var Movie = [movies]()
+    var selectedIndex = -1
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
@@ -23,7 +65,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         return cell
     }
     
-    var ref: DatabaseReference!
+    
 
     @IBOutlet weak var textFieldOutlet: UITextField!
     @IBOutlet weak var tableViewOutlet: UITableView!
@@ -42,6 +84,32 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             self.tableViewOutlet.reloadData()
             print("hello")
         }
+        
+        ref.child("Movies").observe(.childChanged) { snapshot in
+            
+            var name = snapshot.value as! String
+            
+            for i in 0 ..< self.movies.count{
+                if self.movies[i].key == snapshot.key{
+                    self.movies[i] = dict["name"] as! String
+                    self.tableViewOutlet.reloadData()
+                    break
+                }
+            }
+            self.tableViewOutlet.reloadData()
+        }
+        
+        ref.child("students2").observe(.childRemoved) { snapshot in
+            
+            var name = snapshot.value as! String
+                    for i in 0 ..< self.movies.count{
+                if self.students[i].key == snapshot.key{
+                    self.students.remove(at: i)
+                    self.tableViewOutlet.reloadData()
+                    break
+                }
+            }
+        }
     }
 
     @IBAction func addMovieAction(_ sender: Any) {
@@ -50,5 +118,23 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         }
     }
     
+    @IBAction func editAction(_ sender: Any) {
+        movies[selectedIndex] = textFieldOutlet.text!
+       
+        tableViewOutlet.reloadData()
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedIndex = indexPath.row
+    }
+    func deleteFromFireBase(){
+        ref.child("students2").child().removeValue()
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            movies[indexPath.row].deleteFromFirebase()
+            movies.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
 
