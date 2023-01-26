207f3b9c45a1672a8f5e3790f6b40b3e13ce46b2
Christian-bourquin <christian.bourquin23@d155.org>
2023-01-26 07:59:37 -0600

    finale

--- movieListWithFireBase/ViewController.swift
+++ movieListWithFireBase/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by CHRISTIAN BOURQUIN on 1/17/23.
 //
-class movies{
+class Movies{
     var name: String
     var rating: Double
     var ref = Database.database().reference()
@@ -51,17 +51,17 @@ import FirebaseDatabase
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     var ref: DatabaseReference!
     //var names = [String]()
-    var Movie = [movies]()
+    var Movie = [Movies]()
     var selectedIndex = -1
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        return Movie.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "myCell")!
-        cell.textLabel?.text = movies[indexPath.row]
-       // cell.detailTextLabel?.text = "Best movies"
+        cell.textLabel?.text = Movie[indexPath.row].name
+        cell.detailTextLabel?.text = String(Movie[indexPath.row].rating)
         return cell
     }
     
@@ -69,6 +69,8 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
 
     @IBOutlet weak var textFieldOutlet: UITextField!
     @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    @IBOutlet weak var ratingFieldOutlet: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -79,19 +81,23 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         ref = Database.database().reference()
       //  ref.child("Movies").childByAutoId().setValue("harry potter")
         ref.child("Movies").observe(.childAdded) { snapshot in
-            var name = snapshot.value as! String
-            self.movies.append(name)
+            
+            var dict = snapshot.value as! [String: Any]
+            var stews = Movies(dict: dict)
+            stews.key = snapshot.key
+            self.Movie.append(stews)
             self.tableViewOutlet.reloadData()
-            print("hello")
+            
         }
         
         ref.child("Movies").observe(.childChanged) { snapshot in
             
-            var name = snapshot.value as! String
+            var dict = snapshot.value as! [String: Any]
             
-            for i in 0 ..< self.movies.count{
-                if self.movies[i].key == snapshot.key{
-                    self.movies[i] = dict["name"] as! String
+            for i in 0 ..< self.Movie.count{
+                if self.Movie[i].key == snapshot.key{
+                    self.Movie[i].name = dict["name"] as! String
+                    self.Movie[i].rating = dict["rating"] as! Double
                     self.tableViewOutlet.reloadData()
                     break
                 }
@@ -101,10 +107,10 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         
         ref.child("students2").observe(.childRemoved) { snapshot in
             
-            var name = snapshot.value as! String
-                    for i in 0 ..< self.movies.count{
-                if self.students[i].key == snapshot.key{
-                    self.students.remove(at: i)
+            var dict = snapshot.value as! [String: Any]
+            for i in 0 ..< self.Movie.count{
+                if self.Movie[i].key == snapshot.key{
+                    self.Movie.remove(at: i)
                     self.tableViewOutlet.reloadData()
                     break
                 }
@@ -113,26 +119,26 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
 
     @IBAction func addMovieAction(_ sender: Any) {
-        if textFieldOutlet.text != ""{
-            ref.child("Movies").childByAutoId().setValue(textFieldOutlet.text)
-        }
+        let name = textFieldOutlet.text!
+        let rating = Double(ratingFieldOutlet.text!)!
+        var stew = Movies(name: name, rating: rating)
+        stew.saveToFireBase()
     }
     
     @IBAction func editAction(_ sender: Any) {
-        movies[selectedIndex] = textFieldOutlet.text!
-       
+        Movie[selectedIndex].name = textFieldOutlet.text!
+        Movie[selectedIndex].rating = Double(ratingFieldOutlet.text!)!
+        Movie[selectedIndex].updateFirebase()
         tableViewOutlet.reloadData()
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedIndex = indexPath.row
     }
-    func deleteFromFireBase(){
-        ref.child("students2").child().removeValue()
-    }
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete{
-            movies[indexPath.row].deleteFromFirebase()
-            movies.remove(at: indexPath.row)
+            Movie[indexPath.row].deleteFromFirebase()
+            Movie.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
